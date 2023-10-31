886bef079b06438e50e18de5a4141db5704bacd5
satpreet07 <satpreetvirdi007@gmail.com>
2023-10-31 07:03:32 +0530

    Fetching and listing done

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import ToasterProvider from './providers/ToasterProvider'
 import LoginModal from './components/modals/LoginModal'
 import getCurrentUser from './actions/getCurrentUser'
 import RentModal from './components/modals/RentModal'
+import ClientOnly from './components/ClientOnly'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,12 +29,16 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ClientOnly>
         <ToasterProvider/>
         <RentModal />
         <LoginModal />
         <RegisterModal />
         <Navbar currentUser={currentUser} />
-        {children}
+        </ClientOnly>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
         </body>
     </html>
   )
